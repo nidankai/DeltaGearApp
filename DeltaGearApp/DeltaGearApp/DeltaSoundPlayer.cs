cdc7748e1bca1bb297b738e2f969e140fde561f1
nidankai <b4068@oic.jp>
2018-07-04 21:56:39 +0900

    音声認識完了時に完了音と待機音が同時に再生されるように対応（仮対応）

--- DeltaGearApp/DeltaGearApp/DeltaSoundPlayer.cs
+++ DeltaGearApp/DeltaGearApp/DeltaSoundPlayer.cs
@@ -24,13 +24,15 @@ namespace DeltaGearApp
             Max
         };
         private SoundPool _soundPool;
+        private SoundPool _soundPool2;
         private int[] soundIds = new int[3];
 
         public DeltaSoundPlayer()
         {
             _soundPool = new SoundPool(1, Stream.Music, 0);
+            _soundPool2 = new SoundPool(1, Stream.Music, 0);
             soundIds[0] = _soundPool.Load(Application.Context, Resource.Raw.delta_clicked_to_standing_by, 0);
-            soundIds[1] = _soundPool.Load(Application.Context, Resource.Raw.delta_waiting, 0);
+            soundIds[1] = _soundPool2.Load(Application.Context, Resource.Raw.delta_waiting, 0);
             soundIds[2] = _soundPool.Load(Application.Context, Resource.Raw.delta_complete, 0);
         }
 
@@ -40,12 +42,12 @@ namespace DeltaGearApp
             {
                 _soundPool.Play(soundIds[num], 1.0f, 1.0f, 1, 0, 1.0f);
             }
+            _soundPool2.Play(soundIds[1], 1.0f, 1.0f, 1, 0, 1.0f);
         }
 
         public void PlaySoundStandingBy()
         {
             PlaySound(0);
-            PlaySound(1);
         }
 
         public void Destroy()
